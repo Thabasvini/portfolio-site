8442701c6fa1d5583551d1b5d0dcc4c448eaac9b
thabasvini <thabu.chittu@gmail.com>
2023-07-06 18:00:09 +0530

    Added animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import { Typography, Container, Grid, Paper, Button } from '@mui/material';
-import { styled, useTheme } from '@mui/system';
+import React, { useEffect, useRef } from 'react';
+import { Typography, Container, Grid, Button, ThemeProvider, createTheme } from '@mui/material';
+import { styled } from '@mui/system';
+import lottie from 'lottie-web';
 import resume from './resume.pdf';
-
-const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(2),
-  marginBottom: theme.spacing(2),
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
-  boxShadow: theme.shadows[3],
-  borderRadius: theme.shape.borderRadius,
-  // Add more styles as needed
-}));
+import animation from '../static/homepage.json';
 
 const Title = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(2),
@@ -24,8 +16,35 @@ const DownloadButton = styled(Button)(({ theme }) => ({
   color: theme.palette.text.primary,
 }));
 
+const AnimationContainer = styled('div')(({ theme }) => ({
+  width: '100%',
+  maxWidth: '100%',
+  margin: '0 auto',
+  [theme.breakpoints.up('sm')]: {
+    float: 'right',
+    marginLeft: theme.spacing(2),
+  },
+}));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4A148C',
+    },
+    secondary: {
+      main: '#6A1B9A',
+    },
+    background: {
+      default: '#1A1B36',
+    },
+    text: {
+      primary: '#FFFFFF',
+    },
+  },
+});
+
 function Home() {
-  const theme = useTheme();
+  const animationContainer = useRef(null);
 
   const handleDownloadResume = () => {
     const link = document.createElement('a');
@@ -35,26 +54,43 @@ function Home() {
     link.click();
   };
 
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current,
+      animationData: animation,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+    });
+
+    return () => {
+      anim.destroy();
+    };
+  }, []);
+
   return (
-    <Container maxWidth="md" sx={{ marginTop: '5rem' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <StyledPaper elevation={3}>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md" sx={{ marginTop: '8rem' }} style={{textAlign: 'justify'}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
             <Title variant="h6">FRONT-END DEVELOPER</Title>
-            <Typography variant="h5" sx={{ marginBottom: '0.5rem' }}>Hey! I'm Thabasvini</Typography>
-            <Typography>
-            I seamlessly blend artistry and technical prowess to create captivating digital 
-            experiences that not only engage users but also leave a lasting impression, 
-            harnessing the power of technology to make a meaningful impact. 
-            Let's collaborate and bring your digital vision to life!
+            <Typography variant="h5" style ={{color: theme.palette.text.primary}} sx={{ marginBottom: '0.5rem' }}>Hey! I'm Thabasvini</Typography>
+            <Typography style ={{color: theme.palette.text.primary}}>
+              I seamlessly blend artistry and technical prowess to create captivating digital 
+              experiences that not only engage users but also leave a lasting impression, 
+              harnessing the power of technology to make a meaningful impact. 
+              Let's collaborate and bring your digital vision to life!
             </Typography>
             <DownloadButton variant="contained" onClick={handleDownloadResume}>
               Download Resume
             </DownloadButton>
-          </StyledPaper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <AnimationContainer ref={animationContainer} />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </ThemeProvider>
   );
 }
 
